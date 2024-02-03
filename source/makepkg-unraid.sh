7676de08e4f2da463b160f4cd47d948e645c9fd2
ich777 <christophhummer@gmail.com>
2024-02-03 22:40:28 +0100

    source/makepkg-unraid.sh aktualisiert

--- source/makepkg-unraid.sh
+++ source/makepkg-unraid.sh
@@ -8,7 +8,10 @@ RCON_VERSION="$(wget -qO- https://api.github.com/repos/gorcon/rcon-cli/releases/
 mkdir -p $TMP_DIR/$VERSION/usr/bin
 cd $TMP_DIR/$VERSION
 cp --parents -R $BASE_DIR/$PLUGIN_NAME/ $TMP_DIR/$VERSION/
-wget -q -O $TMP_DIR/$VERSION/usr/bin/rcon https://github.com/gorcon/rcon-cli/releases/download/v${RCON_VERSION}/rcon-${RCON_VERSION}-amd64_linux.tar.gz
+wget -q -O $TMP_DIR/rcon.tar.gz https://github.com/gorcon/rcon-cli/releases/download/v${RCON_VERSION}/rcon-${RCON_VERSION}-amd64_linux.tar.gz
+tar -C $TMP_DIR --strip-components=1 -xvf $TMP_DIR/rcon.tar.gz
+cp $TMP_DIR/rcon $TMP_DIR/$VERSION/usr/bin/rcon
+rm -f $(ls $TMP_DIR/* 2>/dev/null|grep -v "$VERSION")
 chmod -R 755 $TMP_DIR/$VERSION/
 rm $TMP_DIR/$VERSION/$BASE_DIR/$PLUGIN_NAME/README.md
 makepkg -l y -c y $TMP_DIR/${PLUGIN_NAME}-$VERSION.txz
